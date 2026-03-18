74c8815443fa36b186fe4ad0b08a37f3107db9b0
JohnDupartDev <johndupartdev@gmail.com>
2026-03-17 20:32:31 -0500

    apertura vercel

--- micropulsespa/astro.config.mjs
+++ micropulsespa/astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  // Agrega tu dominio aquí
+  site: 'https://micropulsespa.com',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
