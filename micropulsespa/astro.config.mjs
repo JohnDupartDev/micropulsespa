2e8979eacaea36029bb60f9ed2c1959d5ca801b4
JohnDupartDev <johndupartdev@gmail.com>
2026-03-25 21:55:05 -0500

    reserva y sitemap

--- micropulsespa/astro.config.mjs
+++ micropulsespa/astro.config.mjs
@@ -1,13 +1,16 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap'; // 1. Importamos la integración
 
 // https://astro.build/config
 export default defineConfig({
-  // Agrega tu dominio aquí
+  // Dominio principal para generar las URLs del sitemap
   site: 'https://micropulsespa.com',
+
+  // 2. Añadimos la integración aquí
+  integrations: [sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
